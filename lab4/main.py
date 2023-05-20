9096ec87230f47b6426e9e5c7ff44c60d6b27c13
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-05-20 12:55:31 +0300

    add lab4

---
+++ lab4/main.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_sequence(N, A, n, phi):
+    x = np.linspace(0, 2 * np.pi, N)  # Задаємо проміжок значень x
+    exact_values = A * np.sin(n * x + phi)  # Точні значення функції
+    max_error = 0.05 * A  # Максимальна похибка не більше 5% від амплітуди
+    noisy_values = exact_values + np.random.uniform(-max_error, max_error,
+                                                    N)  # Застосовуємо випадкове відхилення до точних значень
+    return x, noisy_values
+
+
+def arithmetic_mean(values):
+    return np.mean(values)
+
+
+def harmonic_mean(values):
+    return len(values) / np.sum(1.0 / values)
+
+
+def geometric_mean(values):
+    return np.prod(values) ** (1 / len(values))
+
+
+def plot_graph(x, y, title):
+    plt.figure()
+    plt.plot(x, y)
+    plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+def exact_value(x, A, n, phi):
+    return A * np.sin(n * x + phi)
+
+
+def compare_values(approximate, exact):
+    absolute_error = np.abs(approximate - exact)
+    relative_error = absolute_error / np.abs(exact)
+    return absolute_error, relative_error
+
+
+def compare_errors(approximate, exact):
+    absolute_error, relative_error = compare_values(approximate, exact)
+    max_absolute_error = np.max(absolute_error)
+    min_absolute_error = np.min(absolute_error)
+    max_relative_error = np.max(relative_error)
+    min_relative_error = np.min(relative_error)
+    return max_absolute_error, min_absolute_error, max_relative_error, min_relative_error
+
+
+def visualize_results(x, exact, approximate):
+    plt.figure()
+    plt.plot(x, exact, label='Exact')
+    plt.plot(x, np.full_like(x, approximate), label='Approximate')
+    plt.title('Comparison')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+
+N = 800  # Кількість значень в послідовності
+A = 1.0  # Амплітуда
+n = 8  # Параметр n
+phi = np.pi / 4  # Зсув по фазі
+
+x, y = generate_sequence(N, A, n, phi)
+plot_graph(x, y, 'Noisy Sequence')
+
+approximate_mean = arithmetic_mean(y)
+exact_values = exact_value(x, A, n, phi)
+max_absolute, min_absolute, max_relative, min_relative = compare_errors(approximate_mean, exact_values)
+
+print(f'Arithmetic Mean: {approximate_mean}')
+print(f'Max Absolute Error: {max_absolute}')
+print(f'Min Absolute Error: {min_absolute}')
+print(f'Max Relative Error: {max_relative}')
+print(f'Min Relative Error: {min_relative}')
+
+visualize_results(x, exact_values, approximate_mean)
